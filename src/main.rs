59f1b3b24979e659b03a8c9bf03340e3c913c294
Reyk Floeter <contact@reykfloeter.com>
2019-08-26 14:21:16 +0200

    Use background color for blocks

--- src/main.rs
+++ src/main.rs
@@ -489,13 +489,14 @@ fn main() {
 
     if has_colors() {
         start_color();
-        init_pair(1, COLOR_CYAN, COLOR_BLACK);
-        init_pair(2, COLOR_BLUE, COLOR_BLACK);
-        init_pair(3, COLOR_WHITE, COLOR_BLACK);
-        init_pair(4, COLOR_YELLOW, COLOR_BLACK);
-        init_pair(5, COLOR_GREEN, COLOR_BLACK);
-        init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
-        init_pair(7, COLOR_RED, COLOR_BLACK);
+
+        init_pair(1, COLOR_BLACK, COLOR_CYAN);
+        init_pair(2, COLOR_BLACK, COLOR_BLUE);
+        init_pair(3, COLOR_BLACK, COLOR_WHITE);
+        init_pair(4, COLOR_BLACK, COLOR_YELLOW);
+        init_pair(5, COLOR_BLACK, COLOR_GREEN);
+        init_pair(6, COLOR_BLACK, COLOR_MAGENTA);
+        init_pair(7, COLOR_BLACK, COLOR_RED);
     }
 
     engine(tetromino);
